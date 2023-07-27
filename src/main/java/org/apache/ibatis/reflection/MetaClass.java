fc0ac4238d178ce6b2eaef8f8869445b2edace39
liyilin <lylnhdyx@gmail.com>
2023-07-27 16:49:11 +0800

    Reflection包-MetaClass注释

--- src/main/java/org/apache/ibatis/reflection/MetaClass.java
+++ src/main/java/org/apache/ibatis/reflection/MetaClass.java
@@ -27,6 +27,11 @@ import org.apache.ibatis.reflection.invoker.MethodInvoker;
 import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 /**
+ * 元类型，什么意思呢？提供了查看Class细节的能力
+ *     就是在Class类型的基础上，提供了一些针对Class详细信息的操作
+ *     比如判断类中有没有某一个属性，查询类中指定属性的类型等等细节
+ *     可以查看对应的单元测试MetaClassTest
+ *
  * @author Clinton Begin
  */
 public class MetaClass {
@@ -39,20 +44,44 @@ public class MetaClass {
     this.reflector = reflectorFactory.findForClass(type);
   }
 
+  /**
+   * 静态方法创建type类指定的元类型对象
+   * @param type
+   * @param reflectorFactory
+   * @return
+   */
   public static MetaClass forClass(Class<?> type, ReflectorFactory reflectorFactory) {
     return new MetaClass(type, reflectorFactory);
   }
 
+  /**
+   * 指定name属性类型的元类型对象
+   * @param name
+   * @return
+   */
   public MetaClass metaClassForProperty(String name) {
     Class<?> propType = reflector.getGetterType(name);
     return MetaClass.forClass(propType, reflectorFactory);
   }
 
+  /**
+   * 根据name返回对应的属性名，大小写不敏感
+   *     比如存在属性myName, 查询MYName，返回的匹配属性名就是myName
+   * @param name
+   * @return
+   */
   public String findProperty(String name) {
     StringBuilder prop = buildProperty(name, new StringBuilder());
     return prop.length() > 0 ? prop.toString() : null;
   }
 
+  /**
+   * 在findProperty(name)基础上添加了连字符该驼峰的功能
+   *     这是不是数据库连字符字段可以映射到java对象驼峰格式属性的实现方案？
+   * @param name
+   * @param useCamelCaseMapping 设置为true标识连字符改为驼峰形式
+   * @return
+   */
   public String findProperty(String name, boolean useCamelCaseMapping) {
     if (useCamelCaseMapping) {
       name = name.replace("_", "");
@@ -60,14 +89,30 @@ public class MetaClass {
     return findProperty(name);
   }
 
+  /**
+   * 获取属性名称列表，为什么不说是get方法对应的属性名名称呢？
+   *     因为除了存在对应get方法的属性名外，即便没有对应get方法，也会包含该属性名
+   *     那么这边getter什么意思呢？应该是为了对每一个属性都缓存一个获取属性值的方法，所以有对应get方法就用，没有通过GetFieldInvoker创建一个
+   * @return
+   */
   public String[] getGetterNames() {
     return reflector.getGetablePropertyNames();
   }
 
+  /**
+   * 获取属性名称列表，和get一样，这边为每个属性都提供了设置对应属性值的对象
+   *
+   * @return
+   */
   public String[] getSetterNames() {
     return reflector.getSetablePropertyNames();
   }
 
+  /**
+   * 返回name属性类型
+   * @param name
+   * @return
+   */
   public Class<?> getSetterType(String name) {
     PropertyTokenizer prop = new PropertyTokenizer(name);
     if (prop.hasNext()) {
@@ -77,6 +122,11 @@ public class MetaClass {
     return reflector.getSetterType(prop.getName());
   }
 
+  /**
+   * 返回name属性类型
+   * @param name
+   * @return
+   */
   public Class<?> getGetterType(String name) {
     PropertyTokenizer prop = new PropertyTokenizer(name);
     if (prop.hasNext()) {
